1e0bb69fff9390b66f9e33a0a763b2cd609f9e91
DineshBabu-Git <dinesheee144@gmail.com>
2025-12-22 00:41:27 +0530

    File Updated

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,22 +2,31 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (email, link) => {
-    const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: process.env.EMAIL,
-            pass: process.env.EMAIL_PASSWORD
-        }
-    });
+    try {
+        const transporter = nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,
+            port: process.env.EMAIL_PORT,
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS
+            }
+        });
 
-    await transporter.sendMail({
-        to: email,
-        subject: "Password Reset Link",
-        html: `<h3>Password Reset</h3>
+        await transporter.sendMail({
+            to: email,
+            subject: "Password Reset Link",
+            html: `<h3>Password Reset Request Received</h3>
       <p>Click the link below to reset your password:</p>
       <a href="${link}">${link}</a>
       <p>This link will expire in 15 minutes.</p>`
-    });
+        });
+
+        return true;
+    }
+    catch (error) {
+        console.error("Email error:", error.message);
+        return false;
+    }
 };
 
 module.exports = sendEmail;
