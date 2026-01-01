e314559012d04fd6bc597a07efce7ed6c846ae90
DineshBabu-Git <dinesheee144@gmail.com>
2026-01-02 02:18:42 +0530

    File Updated

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -5,9 +5,10 @@ const sendEmail = async (email, resetLink) => {
     try {
         // Create transporter
         const transporter = nodemailer.createTransport({
+            service: "gmail",
             host: process.env.EMAIL_HOST,
             port: process.env.EMAIL_PORT,
-            secure: false, // true for 465, false for other ports
+            secure: true, // true for 465, false for other ports
             auth: {
                 user: process.env.EMAIL,
                 pass: process.env.EMAIL_PASSWORD,
@@ -22,14 +23,15 @@ const sendEmail = async (email, resetLink) => {
             html: `
                 <h2>Password Reset Link</h2>
                 <h3>You requested a password reset from NodeJs Password Reset Flow.<br>Click the link below to reset your password:</h3>
-                <a href="${resetLink}">Reset Password</a>
+                <a href="${resetLink}">${resetLink}</a>
                 <p>This link will expire in 15 minutes.</p>`,
         };
 
         // Send email
         await transporter.sendMail(mailOptions);
         console.log('Password reset email sent successfully');
-    } catch (error) {
+    }
+    catch (error) {
         console.error('Error sending email:', error);
         throw new Error('Email could not be sent');
     }
