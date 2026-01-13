52683ad6f1394bacc6ee09712e9ef2ae86d937f5
DineshBabu-Git <dinesheee144@gmail.com>
2026-01-14 03:13:57 +0530

    File Updated

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,33 +1,47 @@
 
-const nodemailer = require("nodemailer");
+const axios = require("axios");
 
 const sendEmail = async (email, resetLink) => {
     try {
-        const transporter = nodemailer.createTransport({
-            host: process.env.EMAIL_HOST,
-            port: process.env.EMAIL_PORT,
-            secure: false,
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASSWORD
+        const response = await axios.post(
+            "https://send.api.mailtrap.io/api/send",
+            {
+                from: {
+                    email: process.env.MAILTRAP_SENDER,
+                    name: "Password Reset App"
+                },
+                to: [
+                    {
+                        email: email
+                    }
+                ],
+                subject: "Reset Your Password",
+                html: `
+                    <h3>Password Reset Request</h3>
+                    <p>You requested to reset your password.</p>
+                    <p>
+                        <a href="${resetLink}" target="_blank">
+                            Click here to reset your password
+                        </a>
+                    </p>
+                    <p>This link will expire in 15 minutes.</p>
+                `
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${process.env.MAILTRAP_API_TOKEN}`,
+                    "Content-Type": "application/json"
+                }
             }
-        });
+        );
 
-        await transporter.sendMail({
-            from: process.env.EMAIL_FROM,
-            to: email,
-            subject: "Reset Your Password",
-            html: `
-        <p>You requested to reset your password.</p>
-        <p>Click the link below to reset your password:</p>
-        <p><a href="${resetLink}">Click here to reset your password</a></p>
-        <p>This link will expire in 15 minutes.</p>`
-        });
-
-        return true;
+        return response.status === 200;
 
     } catch (error) {
-        console.error("Email sending failed:", error);
+        console.error(
+            "Email sending failed:",
+            error.response?.data || error.message
+        );
         return false;
     }
 };
